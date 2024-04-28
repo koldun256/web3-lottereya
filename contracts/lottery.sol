cbcce20015998b738f3565b8f91d15a099fbc3c8
koldun <eg.sharypov@yandex.ru>
2024-04-28 09:40:39 +0300

    contract v1

---
+++ contracts/lottery.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract Lottery {
+    uint totalPrize;
+    uint ticketPrice;
+    address public manager;
+    address [] public players;
+
+    // Default Constructor
+    constructor(uint _ticketPrice) payable  {
+        manager = msg.sender;
+        totalPrize = msg.value;
+        ticketPrice = _ticketPrice;
+    }
+    event correctpaymentRecieved(uint);
+
+    // Entering the player to the lottery
+    function enter() public payable {
+        require(msg.value == ticketPrice, "Incorrect amount");
+        players.push(msg.sender);
+    }
+
+    // Random Winner Generation algorithm
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, block.number)));
+    }
+
+    // Picking the winner
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        payable(players[index]).transfer(totalPrize);
+        payable(manager).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    // Restricting  modifier added as sender only calling
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    // Return Players Array
+    function getPlayers() public view returns (address[] memory){
+        return players;
+    }
+
+    function getTicketPrice() public view returns (uint){
+        return ticketPrice;
+    }
+
+    function getTotalPrize() public view returns (uint){
+        return totalPrize;
+    }
+}
