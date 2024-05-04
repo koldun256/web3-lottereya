60f5256716187ffe0375de2849d2a39f49b06a41
Vainslav <139143272+Vainslav@users.noreply.github.com>
2024-05-04 17:48:11 +0300

    Update lottery.sol

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,55 +1,57 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
-contract Lottery {
-    uint totalPrize;
-    uint ticketPrice;
-    address public manager;
-    address [] public players;
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-    constructor(uint _ticketPrice) payable  {
-        manager = msg.sender;
-        totalPrize = msg.value;
+contract Lottery{
+    address payable  public manager;
+    address payable[] public players;
+    uint public timenow;
+    uint public endsAfter;
+    uint public prizePool = 0;
+    uint public ticketPrice;
+
+    event LotteryEntered(address indexed player);
+
+    constructor(uint _endsAfter, uint _ticketPrice) payable  {
+        manager = payable(msg.sender);
+        endsAfter = _endsAfter;
+        timenow = block.timestamp;
         ticketPrice = _ticketPrice;
     }
 
-    event correctpaymentRecieved(uint);
-
-    function enter() public payable {
-        require(msg.value == ticketPrice, "Incorrect amount");
-        players.push(msg.sender);
+    fallback() external payable {
+        pickWinner();
+    }
+    receive() external payable {
+        pickWinner();
     }
 
-    // Random Winner Generation algorithm
-    function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, block.number)));
-    }
-
-    // Picking the winner
-    function pickWinner() public restricted {
-        uint index = random() % players.length;
-        payable(players[index]).transfer(totalPrize);
-        payable(manager).transfer(address(this).balance);
-        players = new address[](0);
-    }
-
-    // Restricting  modifier added as sender only calling
-    modifier restricted() {
+    modifier onlyOwner(){
         require(msg.sender == manager);
         _;
     }
 
-    // Return Players Array
-    function getPlayers() public view returns (address[] memory){
-        return players;
+    function enter() public payable {
+        require(msg.value >= ticketPrice, "Incorrect ammount");
+        players.push(payable(msg.sender));
+        prizePool += msg.value;
+        emit LotteryEntered(msg.sender);
     }
 
-    function getTicketPrice() public view returns (uint){
-        return ticketPrice;
+    function random() private view returns (uint){
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, block.number)));
     }
 
-    function getTotalPrize() public view returns (uint){
-        return totalPrize;
+    function pickWinner() public onlyOwner {
+        require(players.length>0, "Not enough players");
+        require(block.timestamp - timenow >=endsAfter, "Lottery is still running");
+        uint256 winnerIndex = random() % players.length;
+        address payable winner = players[winnerIndex];
+        winner.transfer(prizePool);
+        players = new address payable[](0);
+        timenow = block.timestamp;
+        prizePool = 0;
     }
 }
